f54f0adc52ca8ca5b627958beae4ea57ef8d4105
Means88 <means88.x@gmail.com>
2018-08-14 00:26:09 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Just Serializer
+===============
+
+Just a serializer.
+∑
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='just_serializer',
+    version='0.1.0',
+    packages=['just_serializer'],
+    url='https://github.com/JustProject/just-serializer',
+    keywords='serializer',
+    license='MIT',
+    author='means88',
+    author_email='means88.x@gmail.com',
+    description=''
+)
